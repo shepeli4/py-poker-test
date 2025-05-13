1ea6544e5f30dd81e0f900a6873cede2c4fe4cfa
Michael Shepeli <mih.shperling@gmail.com>
2025-05-13 09:31:09 +0700

    Update python_test1.py

--- python_test1.py
+++ python_test1.py
@@ -6,28 +6,30 @@ def _dummy0(hand):
 
 
 def _dummy1(hand, bet):
+    hand = sorted(hand, key=lambda x: int(x[1:]))
     if not str(_your_hand(hand)[0]) in '987653':
         if is_three(hand):
-            if hand.count(hand[0]) == 3:
+            if list(map(lambda x: int(x[1:]), hand)).count(int(hand[0][1:])) == 3:
                 del hand[-2], hand[-1]
             else:
                 del hand[0], hand[1]
             hand.extend([deck.pop(0), deck.pop(0)])
         elif is_pair(hand):
-            if hand.count(hand[0]) == 2:
-                del hand[-3:]
+            if list(map(lambda x: int(x[1:]), hand)).count(int(hand[0][1:])) == 2:
+                del hand[-3], hand[-2], hand[-1]
                 hand.extend([deck.pop(0), deck.pop(0), deck.pop(0)])
-            elif hand.count(hand[2]) == 2:
+            elif list(map(lambda x: int(x[1:]), hand)).count(int(hand[4][1:])) == 2:
+                del hand[0], hand[1], hand[2]
+                hand.extend([deck.pop(0), deck.pop(0), deck.pop(0)])
+            else:
                 del hand[0], hand[-1]
                 hand.extend([deck.pop(0), deck.pop(0)])
-            else:
-                del hand[:3]
-                hand.extend([deck.pop(0), deck.pop(0), deck.pop(0)])
         else:
+
             del hand[randint(0, 4)], hand[randint(0, 3)], hand[randint(0, 2)]
             hand.extend([deck.pop(0), deck.pop(0), deck.pop(0)])
 
-    return [sorted(hand, key=lambda x: int(x[1:])), _your_hand(hand)[0] * 7 - 15 + randint(11, 30)]
+    return [sorted(hand, key=lambda x: int(x[1:])), bet + _your_hand(hand)[0] * 7 - 15 + randint(11, 30)]
 
 
 def _card_input():
@@ -39,7 +41,7 @@ def _card_input():
             li[li.index(card)] = card[0] + data[card[1:]]
     return li
 
-
+'''
 def _card_print(li):
     data = {'11': 'J', '12': 'Q', '13': 'K', '14': 'T'}
     li = sorted(sorted(li, key=lambda x: x[0]), key=lambda x: int(x[1:]))
@@ -58,7 +60,7 @@ def _card_print(li):
     for card in li:
         if card[1:] in data.keys():
             li[li.index(card)] = card[0] + data[card[1:]]
-
+'''
 
 def _your_hand(hand):
     hand_ranks = [
@@ -129,14 +131,15 @@ shuffle(deck)
 player, bot0 = [[], 0], [[], 0]
 for i in range(5):
     player[0].append(deck.pop(0))
-
+'''
 for i in range(5):
     bot0[0].append(deck.pop(0))
-
-#print(player, bot0)
+'''
+bot0 = [['♦9', '♣11', '♥14', '♥8', '♦8'], 0]
+bot0 = [sorted(bot0[0], key=lambda x: int(x[1:])), 0]
 bot0[1] = _dummy0(bot0[0])
 print(bot0, _your_hand(bot0[0]))
-bot = _dummy1(*bot0)
+bot0 = _dummy1(bot0[0], bot0[1])
 print(bot0, _your_hand(bot0[0]))
 #_card_print(player[0])
 #_card_print(bot0[0])
