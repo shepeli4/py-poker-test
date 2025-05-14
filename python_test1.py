50c843dd2ee2693f50dc4786cc285f8ba921b5b7
Michael Shepeli <mih.shperling@gmail.com>
2025-05-14 23:28:44 +0700

    Update python_test1.py

--- python_test1.py
+++ python_test1.py
@@ -1,6 +1,76 @@
 from random import shuffle, randint
 from time import sleep
 
+def _winner_is(hand0, hand1):
+    combination = _your_hand(hand0)[0]
+    high = []
+    if combination in [1, 5, 6, 9]:
+        if max(hand0, key=lambda x: int(x[1:])) > max(hand1, key=lambda x: int(x[1:])):
+            return 0 #Выигрыш игрока
+        elif max(hand0, key=lambda x: int(x[1:])) < max(hand1, key=lambda x: int(x[1:])):
+            return 1 #Выигрыш бота
+        else:
+            return -1 #Ничья
+
+    elif combination in [2, 3]:
+        hand0, hand1 = sorted([int(i[1:]) for i in hand0]), sorted([int(i[1:]) for i in hand1])
+
+        for i in hand0:
+            if hand0.count(i) == 2 and i not in high:
+                high.append(i)
+        if len(high) == 2:
+            high[0] = max(high)
+            del high[1]
+
+        for i in hand1:
+            if hand1.count(i) == 2 and i not in high:
+                high.append(i)
+        if len(high) == 3:
+            high[1] = max(high[1:])
+            del high[2]
+
+        if high[0] == high[1]:
+            return -1
+        elif max(high) == high[0]:
+            return 0
+        else:
+            return 1
+
+    elif combination in [4, 7]:
+        hand0, hand1 = sorted([int(i[1:]) for i in hand0]), sorted([int(i[1:]) for i in hand1])
+
+        for i in hand0:
+            if hand0.count(i) == 3 and i not in high:
+                high.append(i)
+                break
+
+        for i in hand0:
+            if hand0.count(i) == 3 and i not in high:
+                high.append(i)
+                break
+
+        if max(high) == high[0]:
+            return 0
+        else:
+            return 1
+
+    elif combination == 8:
+        hand0, hand1 = sorted([int(i[1:]) for i in hand0]), sorted([int(i[1:]) for i in hand1])
+
+        for i in hand0:
+            if hand0.count(i) == 4:
+                high.append(i)
+                break
+
+        for i in hand0:
+            if hand0.count(i) == 4:
+                high.append(i)
+                break
+
+        if max(high) == high[0]:
+            return 0
+        else:
+            return 1
 
 def _dummy0(hand):
     return _your_hand(hand)[0] * 5 - 10 + randint(11, 30)
@@ -17,21 +87,19 @@ def _dummy1(hand, bet):
             hand.extend([deck.pop(0), deck.pop(0)])
         elif is_pair(hand):
             if list(map(lambda x: int(x[1:]), hand)).count(int(hand[0][1:])) == 2:
-                del hand[-3], hand[-2], hand[-1]
-                hand.extend([deck.pop(0), deck.pop(0), deck.pop(0)])
+                del hand[-2], hand[-1]
             elif list(map(lambda x: int(x[1:]), hand)).count(int(hand[4][1:])) == 2:
-                del hand[0], hand[1], hand[2]
-                hand.extend([deck.pop(0), deck.pop(0), deck.pop(0)])
+                del hand[0], hand[1]
             else:
                 del hand[0], hand[-1]
-                hand.extend([deck.pop(0), deck.pop(0)])
+            hand.extend([deck.pop(0), deck.pop(0)])
         else:
 
             del hand[randint(0, 4)], hand[randint(0, 3)], hand[randint(0, 2)]
             hand.extend([deck.pop(0), deck.pop(0), deck.pop(0)])
 
     quick_bet = _your_hand(hand)[0] * 7 - 30 + randint(0, 50)
-    while bet - quick_bet < 0 or quick_bet < 0:
+    while bet - quick_bet < 10 or quick_bet < 0:
         quick_bet = _your_hand(hand)[0] * 7 - 30 + randint(0, 50)
     print(f"The bot made a bet of {quick_bet} points")
 
@@ -131,16 +199,11 @@ def is_pair(li):  # 8
     return cnt == 1
 
 
-deck = ['♥' + str(i) for i in range(2, 15)] + ['♦' + str(i) for i in range(2, 15)] + ['♣' + str(i) for i in
-                                                                                      range(2, 15)] + ['♠' + str(i) for
-                                                                                                       i in
-                                                                                                       range(2, 15)]
-shuffle(deck)
 
-
-def __game(pl_bet, bot_bet):
+def __game(pl_bet, bot0_bet):
     bet = 0
-    player, bot0 = [[], pl_bet], [[], bot_bet]
+    player, bot0 = [[], pl_bet], [[], bot0_bet]
+
     for i in range(5):
         player[0].append(deck.pop(0))
 
@@ -158,8 +221,8 @@ def __game(pl_bet, bot_bet):
     print("Place a bet")
 
     quick_bet = int(input())
-    while player[1] - quick_bet < 0:
-        print(f"You have {player[1]} points left, idiot")
+    while player[1] - quick_bet < -1 or quick_bet < 10:
+        print(f"The bet must be more than 10 and not exceed your balance({player[1]} points on your balance).")
         quick_bet = int(input())
     player[1] = player[1] - quick_bet
     bet += quick_bet
@@ -178,8 +241,16 @@ def __game(pl_bet, bot_bet):
     print(" 1  2  3  4  5")
     _card_print(player[0])
 
-    for i in [player[0][int(i) - 1] for i in input().split()]:
-        del player[0][player[0].index(i)]
+    quick_bet = [input(), True]
+    while quick_bet[1]:
+        try:
+            for i in [player[0][int(i) - 1] for i in quick_bet[0].split()]:
+                del player[0][player[0].index(i)]
+            quick_bet[1] = False
+        except:
+            print('Incorrect input. Please enter numbers from 1 to 5 separated by spaces.')
+            quick_bet = [input(), True]
+
     for i in range(5 - len(player[0])):
         player[0].append(deck.pop(0))
 
@@ -188,10 +259,15 @@ def __game(pl_bet, bot_bet):
     print(*_your_hand(player[0]), sep=' - ')
     sleep(0.5)
 
+    print('Do you want to give up?(y/n)')
+    if 'y' in input().lower():
+        return player[1], bot0[1]
+    sleep(0.5)
+
     print("Place a bet")
     quick_bet = int(input())
-    while player[1] - quick_bet < 0:
-        print(f"You have {player[1]} points left, idiot")
+    while player[1] - quick_bet < -1 or quick_bet < 1:
+        print(f"The rate must be positive and not exceed your balance({player[1]} points on your balance).")
         quick_bet = int(input())
     player[1] = player[1] - quick_bet
     bet += quick_bet
@@ -229,25 +305,38 @@ def __game(pl_bet, bot_bet):
     print(*_your_hand(bot0[0]), sep=' - ')
 
     if _your_hand(player[0]) > _your_hand(bot0[0]):
+        print('You win!')
         player[1] += bet
     elif _your_hand(player[0]) < _your_hand(bot0[0]):
+        print('You lose!')
         bot0[1] += bet
     else:
-        print("You all are missing everything (i haven't finished this part yet, wait pls☺)")
+        if _winner_is(player[0], bot0[0]) == 0:
+            print('You win!')
+            player[1] += bet
+        elif _winner_is(player[0], bot0[0]) == 1:
+            print('You lose!')
+            bot0[1] += bet
+        else:
+            print('Draw')
+            player[1] += bet // 2
+            bot0[1] += bet // 2
+
 
     return player[1], bot0[1]
 
 
 try:
-
     print("\033[3mHello player! Welcome to the game \033[93mPoker\033[0m\033[3m! In this game you will play \033[95mclassic poker (Draw Poker)\033[0m\033[3m with a bot.")
     sleep(0.5)
     print("You will be in the game until you lose everything or until you want to stop.")
-    sleep(2)
+    sleep(0.5)
     flag = True
 
     _player_bet, _bot_bet = 100, 99999
     while flag and _player_bet:
+        deck = ['♥' + str(i) for i in range(2, 15)] + ['♦' + str(i) for i in range(2, 15)] + ['♣' + str(i) for i in range(2, 15)] + ['♠' + str(i) for i in range(2, 15)]
+        shuffle(deck)
         if _bot_bet < 1:
             print('Bro... You destroyed our casino. We have no more money.\033[91m GET OUT!!\033[0m')
             break
@@ -256,7 +345,7 @@ try:
 
         if _player_bet > 0:
             print(f"On your account {_player_bet} points. Again? (y/n)")
-            flag = True if input() == 'y' else False
+            flag = True if 'y' in input().lower() else False
     else:
         if _player_bet:
             print(f"You have {_player_bet} points left. Thanks for playing, come again.")
